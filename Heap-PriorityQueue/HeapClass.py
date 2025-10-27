57c08c1aeea8a9e10a14751bbb96d2568d22f4e1
Hrishikesh Sawant <44683244+senorbeast@users.noreply.github.com>
2025-10-27 11:15:08 +0530

    Update comment on sorting in HeapClass.py
    Clarified the time complexity of sorting in the comments.

--- Heap-PriorityQueue/HeapClass.py
+++ Heap-PriorityQueue/HeapClass.py
@@ -123,7 +123,7 @@ class Heap:
 
     # TC = O(n)  [Heapify, build min/max heap from array]
 
-    # Sorting is done in O(n)
+    # Sorting is done in O(n), (not actually)
     # But if we want an array from the heap, 
     # Popping required O(logn) for each element
     # Therefore, sorting an array requires O(nlogn) with heapify and popping to array in order.
